3860757e815a8dfb4ebdd5b966a1d151ccc9eb60
Andrey Bedretdinov <andrew8972@mail.ru>
2021-09-19 11:11:39 +0300

    .

--- Classwork 14.09.21/Бедретдинов. 6. Системы счисления.py
+++ Classwork 14.09.21/Бедретдинов. 6. Системы счисления.py
@@ -1,7 +1,17 @@
-a = int(input('Введите число: '))
+a = int(input('\nВведите число: '))
 x = int(input('Введите конечную СС: '))
-k = ''
+
+answer = ''
 while a != 0:
-    k = str(a % x) + k
+    answer = str(a % x) + answer
     a //= x
-print(k)
+print(f'\nЧисло в {x} СС - {answer}')
+
+k = 0
+for i in range(0, 10):
+    i = str(i)
+    for number in answer:
+        if number == i:
+            k += 1
+            break
+print(f'Различных цифр - {k}')
