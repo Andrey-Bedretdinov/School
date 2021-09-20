220fee4d5de27128a7424f1f39cb17069d41d513
Andrey Bedretdinov <andrew8972@mail.ru>
2021-09-20 20:06:26 +0300

    .

--- Homework 21.09.21/Бедретдинов. 7. Цифры - делители.py
+++ Homework 21.09.21/Бедретдинов. 7. Цифры - делители.py
@@ -1,21 +1,10 @@
-def f7(x_):
-    r_ = []
-    x_ = str(x_)
-    for i_ in x_:
-        r_.append(int(i_))
-    if 0 in r_:
-        return False
-    x_ = int(x_)
-    for div in r_:
-        if x_ % div == 0:
+a = int(input('Введите число: '))
+for number in range(a + 1):
+    for i in str(number):
+        if i == '0':
+            break
+        if number % int(i) == 0:
             continue
-        return False
-    return True
-
-
-a = int(input('Введите натуральное число: '))
-numbers = []
-for i in range(a + 1):
-    if f7(i):
-        numbers.append(i)
-print(numbers)
+        break
+    else:
+        print(number)
