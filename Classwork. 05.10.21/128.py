29f6b2c33e0305482f00a6e8eaaa16ff8b55a0e8
Andrey Bedretdinov <andrew8972@mail.ru>
2021-10-05 12:41:19 +0300

    .

---
+++ Classwork. 05.10.21/128.py
@@ -0,0 +1,9 @@
+string = '2' * 3 + '5' * 18
+while '222' in string or '888' in string:
+    if '555' in string:
+        string = string.replace('555', '8', 1)
+    if '222' in string:
+        string = string.replace('222', '8', 1)
+    else:
+        string = string.replace('888', '2', 1)
+print(string)
