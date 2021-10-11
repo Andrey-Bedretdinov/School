caad68d90352b31a33e5b2686d24cf0502b0ead7
Andrey Bedretdinov <andrew8972@mail.ru>
2021-10-11 19:31:00 +0300

    .

--- Homework. 12.10.21/Number 3.py
+++ Homework. 12.10.21/Number 3.py
@@ -1,11 +1,10 @@
 # Способ 1
 def f(n):
     if n < 1:
-        return 1
+        return n
     elif n % 2 == 0:
         return n + 3 * f(n - 3)
-    else:
-        return 5 * n + 2 * f(n - 5)
+    return 5 * n + 2 * f(n - 5)
 
 
 print(f(30))
