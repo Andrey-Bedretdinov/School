e90452d2ba23e4211d34d9f0284ce0bab6e9bfea
Andrey Bedretdinov <andrew8972@mail.ru>
2021-10-12 11:26:04 +0300

    .

--- Homework. 12.10.21/Number 3.py
+++ Homework. 12.10.21/Number 3.py
@@ -10,3 +10,18 @@ def f(n):
 print(f(30))
 
 # Способ 2
+F = [1] * 31
+for i in range(1, 31):
+    if i < 1:
+        F[i] = 1
+    elif i % 2 == 0:
+        if i < 3:
+            F[i] = i + 3 * (i - 3)
+        else:
+            F[i] = i + 3 * F[i - 3]
+    else:
+        if i < 5:
+            F[i] = 5 * i + 2 * (i - 5)
+        else:
+            F[i] = 5 * i + 2 * F[i - 5]
+print(F[30])
