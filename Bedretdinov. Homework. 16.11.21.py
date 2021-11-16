278ba88037bebdccd308956082f1f379e509fcac
Andrey Bedretdinov <andrew8972@mail.ru>
2021-11-16 11:27:13 +0300

    .

--- Bedretdinov. Homework. 16.11.21.py
+++ Bedretdinov. Homework. 16.11.21.py
@@ -1,3 +1,18 @@
+def is_simple(a):
+    for dev in range(2, int(a**0.5) + 1):
+        if i % dev == 0:
+            return False
+    return True
+
+
+def summa(a):
+    ans = 0
+    while a != 0:
+        ans += a % 10
+        a //= 10
+    return ans
+
+
 print('\nNumber 1')
 for i in range(126849, 126871 + 1):
     divs = [1, i]
@@ -29,3 +44,19 @@ for i in range(1820348, 2880927 + 1):
     if len(divs) == 5:
         divs.sort()
         print(*divs)
+
+print('-------------------')
+print('Number 3')
+count = 0
+for i in range(2943444, 2943529 + 1):
+    count += 1
+    if is_simple(i):
+        print(count, i)
+
+print('-------------------')
+print('Number 4')
+numbers = []
+for i in range(33333, 55555 + 1):
+    if is_simple(i) and summa(i) > 35:
+        numbers.append(i)
+print(*numbers)
