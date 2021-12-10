fbbd077226cb7223e31813ffa66d54eac3270026
Andrey Bedretdinov <andrew8972@mail.ru>
2021-12-10 23:59:45 +0300

    .

--- ЕГЭ/Вариант 25005908/Вариант 25005908.py
+++ ЕГЭ/Вариант 25005908/Вариант 25005908.py
@@ -110,7 +110,7 @@ print(count, minim)
 print('---------')
 
 
-def muves(p):
+def moves(p):
     return p * 2, p * 3
 
 
@@ -118,15 +118,15 @@ def muves(p):
 def f(p):
     if p >= 100:
         return 'WIN'
-    if any(f(x) == 'WIN' for x in muves(p)):
+    if any(f(x) == 'WIN' for x in moves(p)):
         return 'В1'
-    if all(f(x) == 'В1' for x in muves(p)):
+    if all(f(x) == 'В1' for x in moves(p)):
         return 'П1'
-    if any(f(x) == 'П1' for x in muves(p)):
+    if any(f(x) == 'П1' for x in moves(p)):
         return 'В2'
-    if all(f(x) == 'В2' or f(x) == 'В1' for x in muves(p)):
+    if all(f(x) == 'В2' or f(x) == 'В1' for x in moves(p)):
         return 'П2'
-    if any(f(x) == 'П2' or f(x) == 'П1' for x in muves(p)):
+    if any(f(x) == 'П2' or f(x) == 'П1' for x in moves(p)):
         return 'В3'
 
 
