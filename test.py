2a5203e8be60a23d526129a8b603061a361c375b
Andrey Bedretdinov <andrew8972@mail.ru>
2021-12-15 16:42:45 +0300

    .

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-# 1420
 from functools import lru_cache
 
 
 def moves(p):
-    a, b = p
-    return (a + 1, b), (a * 2, b), (a, b + 1), (a, b * 2)
+    return p + 1, p * 2, p * 3
 
 
 @lru_cache()
 def f(p):
-    if p[0] * p[1] >= 63:
+    if p > 72:
+        return 'В1'
+    if p >= 43:
         return 'WIN'
     if any(f(x) == 'WIN' for x in moves(p)):
         return 'В1'
@@ -19,20 +19,9 @@ def f(p):
         return 'В2'
     if all(f(x) == 'В2' or f(x) == 'В1' for x in moves(p)):
         return 'П2'
+    if any(f(x) == 'П2' or f(x) == 'П1' for x in moves(p)):
+        return 'В3'
 
 
-for i in range(1, 35):
-    if f((2, i)) == 'П1':
-        print('#19:', i)
-
-ans = []
-for i in range(1, 35):
-    if f((2, i)) == 'В2':
-        ans.append(i)
-print('#20:', min(ans), max(ans))
-
-ans = []
-for i in range(1, 35):
-    if f((2, i)) == 'П2':
-        ans.append(i)
-print('#20:', max(ans))
+for i in range(1, 100):
+    print(i, f(i))
