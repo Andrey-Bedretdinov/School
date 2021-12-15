1502822e4eef82a5692830fe62864426b2fcc0aa
Andrey Bedretdinov <andrew8972@mail.ru>
2021-12-16 00:14:03 +0300

    .

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
-from functools import lru_cache
+a = 17
+print(f'Мне {a} лет')
 
 
-def moves(p):
-    return p + 1, p * 2, p * 3
 
 
-@lru_cache()
-def f(p):
-    if p > 72:
-        return 'В1'
-    if p >= 43:
-        return 'WIN'
-    if any(f(x) == 'WIN' for x in moves(p)):
-        return 'В1'
-    if all(f(x) == 'В1' for x in moves(p)):
-        return 'П1'
-    if any(f(x) == 'П1' for x in moves(p)):
-        return 'В2'
-    if all(f(x) == 'В2' or f(x) == 'В1' for x in moves(p)):
-        return 'П2'
-    if any(f(x) == 'П2' or f(x) == 'П1' for x in moves(p)):
-        return 'В3'
-
-
-for i in range(1, 100):
-    print(i, f(i))
