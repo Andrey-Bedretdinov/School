21e0807972ed55e8de23c857014e473659931d4d
Andrey Bedretdinov <andrewbedretdinov0@gmail.com>
2022-01-16 19:35:25 +0300

    .

--- Other/test.py
+++ Other/test.py
@@ -1,12 +1,10 @@
-import posix
-
-with open('test.txt', 'r') as file:
-    data = file.read().replace('\n', ' ').replace('(', ' ').split(' ')
-    defs = []
-    for i in range(len(data)):
-        if data[i] == 'def':
-            defs.append(data[i + 1])
-
-defs = list(x.replace('_', ' ').title().replace(' ', '') + 'Error' for x in defs)
-print(*defs)
-
+links_soccerway = []
+with open('links_soccerway.txt', 'r') as file:
+    for line in file:
+        links_soccerway.append(line.strip())
+result = []
+for link in links_soccerway:
+    date = link.split('/')
+    date = f'{date[4]}-{date[5]}-{date[6]}'
+    match_id = link.split('/')[-2]
+    print(f'http://192.168.31.95:12320/soccerway_parser_match/date={date}&id={match_id}')
