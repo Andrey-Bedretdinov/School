346e7664f9c0582596d6828842b25fc69b50ef21
Andrey Bedretdinov <andrewbedretdinov0@gmail.com>
2022-02-15 13:02:12 +0300

    .

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ def f(p):
         return 'В3'
 
 
+print('----------')
 for i in range(1, 55):
     if i * 2 * 2 + 5 >= 59:
         print(i)
@@ -35,5 +36,81 @@ for i in range(1, 55):
 ans = []
 for i in range(1, 55):
     if f((5, i)) == 'П2':
+        ans.append(i)
+print(*ans)
+
+
+def moves(p):
+    return p + 1, p + 5, p * 3
+
+
+@lru_cache(None)
+def f(p):
+    if p >= 59:
+        return 'WIN'
+    if any(f(x) == 'WIN' for x in moves(p)):
+        return 'В1'
+    if all(f(x) == 'В1' for x in moves(p)):
+        return 'П1'
+    if any(f(x) == 'П1' for x in moves(p)):
+        return 'В2'
+    if all(f(x) == 'В2' or f(x) == 'В1' for x in moves(p)):
+        return 'П2'
+    if any(f(x) == 'П2' or f(x) == 'П1' for x in moves(p)):
+        return 'В3'
+
+
+print('----------')
+for i in range(1, 45):
+    if f(i) == 'П1':
+        print(i)
+        break
+
+for i in range(1, 45):
+    if f(i) == 'В2':
+        print(i)
+        break
+
+ans = []
+for i in range(1, 45):
+    if f(i) == 'П2':
+        ans.append(i)
+print(*ans)
+
+
+def moves(p):
+    return p + 1, p + 5, p * 3
+
+
+@lru_cache(None)
+def f(p):
+    if p >= 59:
+        return 'WIN'
+    if any(f(x) == 'WIN' for x in moves(p)):
+        return 'В1'
+    if all(f(x) == 'В1' for x in moves(p)):
+        return 'П1'
+    if any(f(x) == 'П1' for x in moves(p)):
+        return 'В2'
+    if all(f(x) == 'В2' or f(x) == 'В1' for x in moves(p)):
+        return 'П2'
+    if any(f(x) == 'П2' or f(x) == 'П1' for x in moves(p)):
+        return 'В3'
+
+
+print('----------')
+for i in range(1, 45):
+    if f(i) == 'П1':
+        print(i)
+        break
+
+for i in range(1, 45):
+    if f(i) == 'В2':
+        print(i)
+        break
+
+ans = []
+for i in range(1, 45):
+    if f(i) == 'П2':
         ans.append(i)
 print(*ans)
