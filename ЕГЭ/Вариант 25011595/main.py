4ccf8c64b3752369f3052076e777ccc0b05e2b3a
Andrey Bedretdinov <andrewbedretdinov0@gmail.com>
2022-06-19 15:26:57 +0300

    .

--- ЕГЭ/Вариант 25011595/main.py
+++ ЕГЭ/Вариант 25011595/main.py
@@ -160,6 +160,88 @@ for i in range(1000, 1, -1):
         break
 
 
+# Number 23
+def f(start, stop):
+    if start > stop:
+        return 0
+    if start == stop:
+        return 1
+    return f(start + 2, stop) + f(start * 2, stop)
+
+
+print(f(1, 18) * f(18, 52))  # 96
+
+
+# Number 24
+with open('24 (1).txt') as file:
+    data = file.readline()
+
+i = 0
+count = 0
+maxi = 0
+while i < len(data) - 1:
+    ab = data[i] + data[i + 1]
+    if ab in ['AB', 'CB']:
+        count += 1
+        maxi = max(maxi, count)
+        i += 2
+    else:
+        count = 0
+        i += 1
+print(maxi)
+
+
+# Number 25
+for x in range(10):
+    for y in range(10):
+        a = int(f'12345{x}7{y}8')
+        if a % 23 == 0 and a <= 10**9:
+            print(a, a // 23)
+
+# 123450798 5367426
+# 123451718 5367466
+# 123453788 5367556
+# 123454708 5367596
+# 123456778 5367686
+# 123459768 5367816
+
+
+# Number 26
+with open('26.txt') as file:
+    n = int(file.readline())
+    data = []
+    for _ in range(n):
+        line = [int(x) for x in file.readline().strip('\n').split(' ')]
+        data.append(line)
+
+xy = {}
+for i in data:
+    if not xy.get(i[0]):
+        xy[i[0]] = []
+    xy[i[0]].append(i[1])
+    xy[i[0]].sort()
+
+
+maxi_x = 0
+mini_y = 10**20
+for x in xy:
+    if len(xy[x]) < 2:
+        continue
+
+    for i in range(len(xy[x]) - 1):
+        a, b = xy[x][i], xy[x][i + 1]
+        if b - a - 1 == 3:  # Поменять на 13!!!
+            maxi_x = x
+            mini_y = a + 1
+            break
+
+print(maxi_x, mini_y)  # 56588 67073
+
+
+
+
+
+
 
 
 
--- ЕГЭ/Вариант 25011595/test.py
+++ ЕГЭ/Вариант 25011595/test.py
@@ -1,13 +1,31 @@
-# Number 23
-def f(start, stop):
-    if start > stop:
-        return 0
-    if start == stop:
-        return 1
-    return f(start + 2, stop) + f(start * 2, stop)
+# Number 26
+with open('26.txt') as file:
+    n = int(file.readline())
+    data = []
+    for _ in range(n):
+        line = [int(x) for x in file.readline().strip('\n').split(' ')]
+        data.append(line)
+
+xy = {}
+for i in data:
+    if not xy.get(i[0]):
+        xy[i[0]] = []
+    xy[i[0]].append(i[1])
+    xy[i[0]].sort()
 
 
-print(f(1, 18) * f(18, 52))  # 96
+maxi_x = 0
+mini_y = 10**20
+for x in xy:
+    if len(xy[x]) < 2:
+        continue
 
+    for i in range(len(xy[x]) - 1):
+        a, b = xy[x][i], xy[x][i + 1]
+        if b - a - 1 == 3:  # Поменять на 13!!!
+            maxi_x = x
+            mini_y = a + 1
+            break
 
+print(maxi_x, mini_y)  # 56588 67073
 
